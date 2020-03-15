279a4f9891d9dc1acf3437174177da0a11e60bd6
mojtaba mohammadi <49897989+mojtabams@users.noreply.github.com>
2020-03-15 17:40:15 +0330

    Read inputDATA

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//budgetController
 var budgetController = (function () {
     //
     /* return {
@@ -7,22 +8,49 @@ var budgetController = (function () {
     } */
 })();
 
+//UIController
 var UIController = (function () {
-    //
 
-    /* return {
-        : function () {
-            return;
+    var DOMstring = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    }
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstring.inputType).value, //  inc  OR  exp
+                description: document.querySelector(DOMstring.inputDescription).value,
+                value: document.querySelector(DOMstring.inputValue).value,
+            };
+        },
+
+        getDOMstring: function () {
+            return DOMstring;
         }
-    } */
+    };
 
 })();
 
+
+// Global appController
 var Controller = (function (budgetCtr, UICtr) {
-    //
-    /* return {
-        : function () {
-            return;
+    var DOM = UICtr.getDOMstring();
+    var ctrlAddItem = function () {
+        //filed input value
+        var input = UICtr.getInput();
+        console.log(input);
+
+    }
+
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
         }
-    } */
+    });
+
 })(budgetController, UIController);
