8b21a79bfcf601de23b533925145087b5862a8cb
mojtaba mohammadi <49897989+mojtabams@users.noreply.github.com>
2020-03-15 16:10:07 +0330

    create Controllers

---
+++ app.js
@@ -0,0 +1,28 @@
+var budgetController = (function () {
+    //
+    /* return {
+        : function () {
+            return;
+        }
+    } */
+})();
+
+var UIController = (function () {
+    //
+
+    /* return {
+        : function () {
+            return;
+        }
+    } */
+
+})();
+
+var Controller = (function (budgetCtr, UICtr) {
+    //
+    /* return {
+        : function () {
+            return;
+        }
+    } */
+})(budgetController, UIController);
